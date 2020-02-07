cadb8dfb95b7f0f17dd82def6efa3334190abbb5
Dmitry Zaitsev <dmitry.zaitsev.1993@gmail.com>
2020-02-07 15:31:25 +0100

    Main Project

---
+++ TODO App/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TODO_App.Model;
+using TODO_App.Services;
+
+namespace TODO_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        private BindingList<TODO> todoDataList;
+        private FileIOService fileIOService;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DgTodoList_Loaded(object sender, RoutedEventArgs e)
+        {
+            fileIOService = new FileIOService(PATH);
+
+            try
+            {
+                todoDataList = fileIOService.LoadData();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+
+            dgTodoList.ItemsSource = todoDataList;
+            todoDataList.ListChanged += todoDataList_ListChanged;
+        }
+
+        private void todoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if(e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    fileIOService.SaveData(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
+        }
+    }
+}
---
+++ TODO App/Model/TODO.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Newtonsoft.Json;
+
+namespace TODO_App.Model
+{
+    class TODO : INotifyPropertyChanged
+    {
+        [JsonProperty(PropertyName = "creationDate")]
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool isDone;
+        private string text;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected  void OnPropertyChanged (string propertyName = "")
+        {
+            PropertyChanged ? .Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        [JsonProperty(PropertyName = "isDone")]
+        public bool IsDone
+        {
+            get { return isDone; }
+            set {
+                if (isDone == value) return;
+                isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+
+        [JsonProperty(PropertyName = "text")]
+        public string Text
+        {
+            get { return text; }
+            set {
+                if (text == value) return;
+                text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+    }
+}
